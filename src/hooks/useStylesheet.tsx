d31b96613a841246572bdf7933101782e61e9ff6
Samarth Patel <samarthxpatel@gmail.com>
2020-04-27 22:40:48 -0400

    Added service worker and smoothened stylesheet loading

--- src/hooks/useStylesheet.tsx
+++ src/hooks/useStylesheet.tsx
@@ -1,14 +1,25 @@
 import { useEffect } from 'react'
 
 export const useStylesheet = (dark: boolean) => {
-	useEffect( () => {
-		const head = document.head
-		const link = document.createElement('link')
-		link.type = 'text/css'
-		link.rel= 'stylesheet'
-		link.href = dark ? '/dark.css' : '/light.css'
-		head.appendChild(link)
+	useEffect(() => {
+        const newID = `__useStyleSheet${dark ? 'Dark' : 'Light'}__`
+        const oldID = `__useStyleSheet${!dark ? 'Dark' : 'Light'}__`
 
-		return () => { head.removeChild(link) }
+        const newStyleSheetLink = document.getElementById(newID)
+        const oldStyleSheetLink = document.getElementById(oldID)
+
+        if (oldStyleSheetLink)
+            document.head.removeChild(oldStyleSheetLink)
+
+        if (!newStyleSheetLink){
+            const link = document.createElement('link')
+            link.type = 'text/css'
+            link.rel= 'stylesheet'
+            link.href = dark ? '/dark.css' : '/light.css'
+            link.className = newID
+            document.head.appendChild(link)
+
+            return () => { document.head.removeChild(link) }
+        }
 	}, [dark])
 }
